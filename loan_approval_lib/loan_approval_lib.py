a3b71013399091deb68d4ffa59c6cbe724533395
edward <example@example.com>
2025-07-06 15:03:54 +0100

    update library code

--- loan_approval_lib/loan_approval_lib.py
+++ loan_approval_lib/loan_approval_lib.py
@@ -27,6 +27,7 @@ def load_data_sample_submission():
 def load_original_data():
     path = './data/original_data/credit_risk_dataset.csv'
     df = pandas.read_csv(path)
+    df = df[['person_age', 'person_income', 'person_home_ownership', 'person_emp_length', 'loan_intent', 'loan_grade', 'loan_amnt', 'loan_int_rate', 'loan_percent_income', 'cb_person_default_on_file', 'cb_person_cred_hist_length', 'loan_status']]
     return df
     
 
